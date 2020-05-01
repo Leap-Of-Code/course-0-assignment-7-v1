896043a765ab59897a1168bc41bf798667bb6f77
cyndisong28 <30833889+cyndisong28@users.noreply.github.com>
2020-05-01 06:33:53 +0000

    Saving progress for all questions.

--- fix-code-4/main.cpp
+++ fix-code-4/main.cpp
@@ -2,33 +2,45 @@
 #include <string>
 using namespace std;
 
+int GetUserInput() {
+int input;
+cout << "what is your number?";
+cin >> input; 
+return input;
+}
+
 int main() {
-  int number = get_user_input();
+  int number = GetUserInput();
 
   int value = 0;
-  if (number % 1 == 0) {
+  if (1 % number == 0) {
     value++;
   }
-  if (number % 2 == 0) {
+  if (2 % number == 0) {
     value++;
   }
-  if (number % 3 == 0) {
-    value--;
-  }
-  if (number % 4 == 0) {
+  if (3 % number == 0) {
     value++;
   }
-  if (number % 5 == 0) {
+  if (4 % number == 0) {
     value++;
   }
-  if (number % 6 == 0) {
+  if (5 % number == 0) {
     value++;
   }
-  if (number % 7 == 0) {
+  if (6 % number == 0) {
     value++;
   }
-
-  cout << "The number of multiples less than 10 for " << number " is: " << value << endl;
+  if (7 % number == 0) {
+    value++;
+  }
+  if (8 % number == 0) {
+    value++;
+  }
+  if (9 % number == 0) {
+    value++;
+  }
+  cout << "The number of multiples less than 10 for " << number << " is: " << value << endl;
 
   return 0;
 }
