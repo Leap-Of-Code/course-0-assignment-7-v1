313852dd59736b6961809a55b8b0d1979739a5df
cyndisong28 <30833889+cyndisong28@users.noreply.github.com>
2020-05-02 03:05:32 +0000

    Updating all questions

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -4,46 +4,51 @@ using namespace std;
 
 void Evaluator (int input_y) {
 int number_of_output = 0;
+cout << "The number(s) divisible by the input is(are): ";
+  string outputs;
   if (input_y % 1 == 0) {
     number_of_output++;
-    cout << "1, ";
+    outputs = outputs + "1 ,";
   }
   if (input_y % 2 == 0) {
     number_of_output++;
-    cout << "2, ";
+    outputs = outputs + "2, ";
   }
   if (input_y % 3 == 0) {
     number_of_output++;
-    cout << "3, ";
+    outputs = outputs + "3, ";
   }
   if (input_y % 4 == 0) {
     number_of_output++;
-    cout << "4, ";
+    outputs = outputs + "4, ";
   }
   if (input_y % 5 == 0) {
     number_of_output++;
-    cout << "5, ";
+    outputs = outputs + "5, ";
   }
   if (input_y % 6 == 0) {
     number_of_output++;
-    cout << "6, ";
+    outputs = outputs + "6, ";
   }
   if (input_y % 7 == 0) {
     number_of_output++;
-    cout << "7, ";
+    outputs = outputs + "7, ";
   }
   if (input_y % 8 == 0) {
     number_of_output++;
-    cout << "8, ";
+    outputs = outputs + "8, ";
   }
   if (input_y % 9 == 0) {
     number_of_output++;
-    cout << "9, ";
+    outputs = outputs + "9, ";
   }
   if (input_y % 10 == 0) {
     number_of_output++;
-    cout << "10, ";
+    outputs = outputs + "10, ";
   }
+  outputs.pop_back();
+  outputs.pop_back();
+  cout << outputs << ". ";
   cout << "Total number of output is " << number_of_output << ".";
 }
 
