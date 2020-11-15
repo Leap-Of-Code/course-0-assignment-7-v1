72d2df88e800b99a4b07c18dda92c0906ac0fb52
anaygupta2004 <55030203+anaygupta2004@users.noreply.github.com>
2020-11-15 05:04:34 +0000

    Updating all questions

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -2,12 +2,58 @@
 #include <string>
 using namespace std;
 
-int evaluator() {
-
+int evaluator(int number) {
+   int value = 0;
+   if (number % 1 == 0) {
+      value++;
+      cout << "1, ";
+    }
+    if (number % 2 == 0) {
+      value++;
+      cout << "2, ";
+    }
+    if (number % 3 == 0) {
+      value++;
+      cout << "3, ";
+    }
+    if (number % 4 == 0) {
+      value++;
+      cout << "4, ";
+    }
+    if (number % 5 == 0) {
+      value++;
+      cout << "5, ";
+    }
+    if (number % 6 == 0) {
+      value++;
+      cout << "6, ";
+    }
+    if (number % 7 == 0) {
+      value++;
+      cout << "7, ";
+    }
+    if (number % 8 == 0) {
+      value++;
+      cout << "8, ";
+    }
+    if (number % 9 == 0) {
+      value++;
+      cout << "9, ";
+    }
+    if (number % 10 == 0) {
+      value++;
+      cout << "10, ";
+    }
+    return value;
 }
 
 
 int main() {
+  int number;
+  cout << "Number: ";
+  cin >> number;
+  int value = evaluator(number);
+  cout << endl <<  "Multiples: " << value << endl;
   return 0;
 }
 
